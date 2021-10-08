c5387ad5683e49d0f69548d1fbfcd033a0fae676
ImJustLucas <bellierlucas.pro@gmail.com>
2021-10-08 14:19:29 +0200

    🎉 first commit

---
+++ front.misterbin/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import "./assets/scss/main.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Bienvenue sur Mister Bin</p>
+    </div>
+  );
+}
+
+export default App;
