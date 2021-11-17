b9750f0a0d495a3f735f437fdf67feffa7d9bdcc
YounesBessa <bessa460@gmail.com>
2021-11-17 10:06:42 +0100

    ✨ add image component

---
+++ front.misterbin/src/components/Image/index.tsx
@@ -0,0 +1,31 @@
+interface Props {
+  src: string;
+  alt: string;
+  width?: number;
+  height?: number;
+  className?: string;
+}
+
+const Image: React.FC<Props> = ({
+  src,
+  width,
+  height,
+  alt,
+  className,
+  ...props
+}: Props) => {
+  return (
+    <img
+      src={src}
+      width={width}
+      height={height}
+      alt={alt}
+      className={className}
+      loading={"lazy"}
+      draggable={false}
+      {...props}
+    />
+  );
+};
+
+export default Image;
