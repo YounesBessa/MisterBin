c44cac389f0dc98f5cd663d8f3bbf2d1ea2ae7b6
YounesBessa <bessa460@gmail.com>
2021-11-21 23:23:53 +0100

    🚧 add map component

---
+++ front.misterbin/src/components/Map/index.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Map: React.FC = () => {
+  return (
+    <Container>
+      Ce sera la page de la map.
+    </Container>
+  );
+};
+
+const Container = styled.div``;
+
+export default Map;
