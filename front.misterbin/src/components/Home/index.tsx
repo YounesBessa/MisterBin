4449b60d49b5592f46edde6848c77f68183cbef6
Younès Bessa <bessa460@gmail.com>
2021-12-08 09:37:26 +0100

    🎨 add new keyframe and assets to the home page

--- front.misterbin/src/components/Home/index.tsx
+++ front.misterbin/src/components/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import FeaturedCard from "../FeaturedCard";
 import Card from "../Card";
 import Wave from "../Wave";
 import Image from "../Image";
@@ -17,7 +18,7 @@ const Home: React.FC = () => {
               MisterBin
             </Title>
             <FeaturedCard>
-              <Picture
+              <Recycle
                 src="/static/images/recycle.png"
                 alt="Logo recyclage"
                 width={120}
@@ -30,21 +31,38 @@ const Home: React.FC = () => {
         <Wave />
         <WhiteContainer>
           <BottomWrapper>
-            <SecondTitle>LISTE DES VILLES DISPONIBLES</SecondTitle>
-            <CardContainer>
+            <SecondTitleWrapper>
+            <ImageContainer>
+          <Ecologie
+                src="/static/images/ecologie.svg"
+                alt="Logo recyclage"
+                width={120}
+                height={120}
+              />
+          </ImageContainer>
+            </SecondTitleWrapper>
+            <SecondTitle>
+          LISTE DES VILLES DISPONIBLES</SecondTitle>
+            <CardWrapper>
+              <LeftCardContainer>
               <BottomCard>
-                <CardTitle>TOULOUSE</CardTitle>
+                <CardTitle>PARIS</CardTitle>
                 <Button>VOIR</Button>
               </BottomCard>
+              </LeftCardContainer>
+              <CenterCardContainer>
               <BottomCard>
-              <CardTitle>PARIS</CardTitle>
+              <CardTitle>TOULOUSE</CardTitle>
                 <Button disabled>VOIR</Button>
               </BottomCard>
+              </CenterCardContainer>
+              <RightCardContainer>
               <BottomCard>
               <CardTitle>LYON</CardTitle>
                 <Button disabled>VOIR</Button>
               </BottomCard>
-            </CardContainer>
+              </RightCardContainer>
+            </CardWrapper>
           </BottomWrapper>
         </WhiteContainer>
       </Body>
@@ -84,11 +102,8 @@ align-items: center;
 }
 `;
 
-const FeaturedCard = styled(Card)`
-`;
-
 const Title = styled.h1`
-font-family: "Montserrat-Light";
+margin-top: 100px;
 text-align: center;
 width: 30%;
 font-weight: 400;
@@ -105,35 +120,151 @@ const BottomWrapper = styled.div`
 display: flex;
 justify-content: center;
 flex-direction: column;
+margin-top: 100px;
 `;
+
+const ImageContainer = styled.span`
+z-index: 2;
+position: absolute;
+left: -6%;
+top: -30px;
+z-index: 2;
+animation: InfiniteRotation 20s linear infinite;
+`
+
+const SecondTitleWrapper = styled.div`
+position:relative;
+`
+
 const SecondTitle = styled.h2`
-font-family: "Kurale-Regular";
 text-align: center;
 font-weight: 400;
 margin: 0px;
 padding: 0;
 height: fit-content;
-text-decoration: underline;
+font-size: 50px;
+position: relative;
+z-index: 4;
+::after{
+  position: absolute;
+  content: '';
+  width: 480px;
+  height: 2px;
+  background: black;
+  top: 100%;
+  left: 54%;
+}
 `;
 
-const CardContainer = styled.div`
+const CardWrapper = styled.div`
 display: flex;
 height: fit-content;
 width: 100%;
+margin-top: 100px;
 @media screen and (max-width: 767px) {
 flex-direction: column;
 align-items: center;
 }
-
 `;
 
+const LeftCardContainer = styled.div`
+position: relative;
+::before {
+  position: absolute;
+  content: '';
+  width: 180px;
+  height: 180px;
+  background: #2DC653;
+  z-index: 4;
+  border-radius: 50%;
+  top: -40px;
+}
+::after {
+  position: absolute;
+  content: '';
+  width: 180px;
+  height: 180px;
+  background: linear-gradient(133.74deg, #70E000 44.13%, rgba(0, 128, 0, 0) 93.68%);
+  z-index: 4;
+  border-radius: 50%;
+  bottom: -30px;
+  right: 20px;
+}
+`
+
+const CenterCardContainer = styled.div`
+position: relative;
+::before {
+  position: absolute;
+  content: '';
+  width: 180px;
+  height: 180px;
+  background: #2DC653;
+  z-index: 4;
+  border-radius: 50%;
+  top: -40px;
+  right: 90px;
+}
+::after {
+  position: absolute;
+  content: '';
+  width: 180px;
+  height: 180px;
+  background: linear-gradient(133.74deg, #70E000 44.13%, rgba(0, 128, 0, 0) 93.68%);
+  z-index: 4;
+  border-radius: 50%;
+  bottom: -30px;
+  right: 90px;
+}
+`
+
+const RightCardContainer = styled.div`
+position: relative;
+::before {
+  position: absolute;
+  content: '';
+  width: 150px;
+  height: 150px;
+  background: #2DC653;
+  z-index: 4;
+  border-radius: 50%;
+  top: -40px;
+  right: 20px;
+}
+::after {
+  position: absolute;
+  content: '';
+  width: 180px;
+  height: 180px;
+  background: #70E000;
+  background: linear-gradient(133.74deg, #70E000 44.13%, rgba(0, 128, 0, 0) 93.68%);
+  z-index: 4;
+  border-radius: 50%;
+  bottom: -30px;
+  left: 20px;
+}
+
+`
+
 const BottomCard = styled(Card)`
+position: relative;
+position: relative;
+background: linear-gradient(145.56deg, rgba(238, 238, 238, 0.6) 2.7%, rgba(238, 238, 238, 0.2) 50%);
+border: 1px solid rgba(0, 0, 0, 0.2);
+box-sizing: border-box;
+box-shadow: 0px 4px 24px -1px rgba(0, 0, 0, 0.25);
+backdrop-filter: blur(40px);
 display: flex;
-margin: 0 120px;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+width: 220px;
+height: 350px;
+border-radius: 25px;
+z-index:5;
 `;
 
 const CardTitle = styled.p`
-font-family: "Kurale-Regular";
 text-align: center;
 font-weight: 400;
 font-size: 30px;
@@ -143,19 +274,11 @@ padding: 0;
 height: fit-content;
 `;
 
-const Picture = styled(Image)`
+const Recycle = styled(Image)`
 margin-bottom: 40px;
 `;
 
-// const DisabledButton = styled(Button)`
-//     background-color: grey;
-//     :hover {
-//     background-color: white;
-//     color: grey;
-//     transition: all 0.2s;
-//     border-color: grey;
-//   }
-// `  
+const Ecologie = styled(Image)``;
 
 
 export default Home;
