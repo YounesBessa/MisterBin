1f396ca71920ea038a1a56e8c8c98ff26564ba05
Younès Bessa <bessa460@gmail.com>
2021-12-08 10:16:18 +0100

    💄 update card component style

--- front.misterbin/src/components/Card/index.tsx
+++ front.misterbin/src/components/Card/index.tsx
@@ -15,7 +15,11 @@ const Card: React.FC<Props> = ({children}:Props) => {
 };
 
 const Container=styled.div`
-background: linear-gradient(145.56deg, rgba(238, 238, 238, 0.6) 2.7%, rgba(238, 238, 238, 0.2) 97.86%);
+/* background: linear-gradient(145.56deg, rgba(238, 238, 238, 0.6) 2.7%, rgba(238, 238, 238, 0.2) 97.86%); */
+/* top: 40px; */
+margin: 0 70px;
+position: relative;
+background: linear-gradient(145.56deg, rgba(238, 238, 238, 0.6) 2.7%, rgba(238, 238, 238, 0.2) 50%);
 border: 1px solid rgba(0, 0, 0, 0.2);
 box-sizing: border-box;
 box-shadow: 0px 4px 24px -1px rgba(0, 0, 0, 0.25);
@@ -27,10 +31,7 @@ align-items: center;
 width: 220px;
 height: 350px;
 border-radius: 25px;
-z-index:3;
-position: relative;
-top: 40px;
-margin: 0 20px;
+z-index:5;
 @media screen and (max-width: 767px) {
   margin: 30px 0;
   }
